190e2b508a47b6b0738cc313b10cc254126e304a
ajw123 <ajw123@ymail.com>
2022-12-06 12:37:25 -0500

    fixing day 5

--- Day_2/code.py
+++ Day_2/code.py
@@ -18,7 +18,9 @@ points["Z"] = 3 #Scissors
 points["W"] = 6
 points["L"] = 0
 points["D"] = 3
-total = 0
+total_1 = 0
+total_2 = 0
+points_array = [3,1,2]
 
 ##code
 with open(p, 'r') as file:
@@ -41,29 +43,22 @@ with open(p, 'r') as file:
                     outcome_points = "W"
                 else:
                     outcome_points = "L"
-        total = total + points[you] + points[outcome_points]
-print(total)
 
-total = 0
-with open(p, 'r') as file:
-    content = file.readlines()
-    for line in content:
-        outcome_requirement = line[-2].strip()
         opponent = line[:1].strip()
-        match outcome_requirement:
+        match you:
             case "X":
                 outcome_required = "L"
-                you = points[opponent] - 1
-                if you == 0:
-                    you = 3
+                you_points = points_array[points[opponent] - 1]
             case "Y":
                 outcome_required = "D"
-                you = points[opponent]
+                you_points = points[opponent]
             case "Z":
                 outcome_required = "W"
-                you = (points[opponent] % 3) + 1 
+                you_points = (points[opponent] % 3) + 1 
+        total_1 = total_1 + points[you] + points[outcome_points]
+        total_2 = total_2 + you_points + points[outcome_required]
+print(total_1)
+print(total_2)
 
-        total = total + you + points[outcome_required]
-print(total)
 print("--- %s seconds ---" % (time.time() - start_time))
 
--- Day_5/code.py
+++ Day_5/code.py
@@ -15,10 +15,9 @@ total = 0
 ##code
 
 
-arrangement = []
-buckets = {}
-
 def get_stacks(p):
+    buckets = {}
+
     with open(p,'r') as file:
         content = file.readlines()
         for line in content:
@@ -30,20 +29,26 @@ def get_stacks(p):
                 if i not in buckets.keys():
                     buckets[i] = []
                 if  value != '':    
-                    buckets[i].insert(0,value)
+                    buckets[i].insert(0,value.replace("[","").replace("]",""))
                 i = i + 1
         del buckets[0]
         return buckets
 
-def put_movement(stacks,move_amount, from_stack, to_stack):
+def put_movement(stacks,move_amount, from_stack, to_stack,direction):
     move_amount = int(move_amount)
     from_stack = int(from_stack)
     to_stack = int(to_stack)
-    from_amount_start = len(stacks[from_stack])-1
-    from_amount_end = from_amount_start - move_amount
-    print(move_amount)
-    for idx in range(from_amount_start,from_amount_end,-1):
-        stacks[to_stack].append(stacks[from_stack].pop(stacks[from_stack].index(stacks[from_stack][idx])))
+    if direction < 0:
+        from_amount_start = len(stacks[from_stack]) - 1
+        from_amount_end = from_amount_start - move_amount
+        for idx in range(from_amount_start,from_amount_end,direction):
+            stacks[to_stack].append(stacks[from_stack].pop(idx))
+    else:
+        from_amount_end = len(stacks[from_stack])
+        from_amount_start = from_amount_end - move_amount
+       ##print(stacks[from_stack],move_amount,from_amount_start,from_amount_end,stacks[from_stack][from_amount_start])
+        for idx in range(from_amount_start,from_amount_end,direction):
+            stacks[to_stack].append(stacks[from_stack].pop(from_amount_start))    
     return stacks
 
 def get_last_item(stacks):
@@ -52,6 +57,7 @@ def get_last_item(stacks):
         last_element.append(stacks[i][-1])
     return last_element
 
+
 stacks = get_stacks(p)
 
 with open(q,'r') as file:
@@ -59,12 +65,23 @@ with open(q,'r') as file:
     i = 1
     for line in content:
         re_line = line.strip().replace("move ","").replace(" from ",";").replace(" to ",";").split(";")
-        stacks = put_movement(stacks,re_line[0],re_line[1],re_line[2])
+        stacks = put_movement(stacks,re_line[0],re_line[1],re_line[2],-1)
         i = i + 1
-        print(re_line[0],re_line[1],re_line[2],stacks)
-        if i > 4:
-            print(get_last_item(stacks))
-            quit()
-    print(stacks)
     print(get_last_item(stacks))
+
+stacks = {}
+p = Path(__file__).with_name('initial.txt')
+q = Path(__file__).with_name('input.txt')
+
+stacks = get_stacks(p)
+
+with open(q,'r') as file:
+    content = file.readlines()
+    i = 1
+    for line in content:
+        re_line = line.strip().replace("move ","").replace(" from ",";").replace(" to ",";").split(";")
+        stacks = put_movement(stacks,re_line[0],re_line[1],re_line[2],1)
+        i = i + 1
+    print(get_last_item(stacks))
+
 print("--- %s seconds ---" % (time.time() - start_time))
